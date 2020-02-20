8bd5bcb28688d411cd16b5e103dda399a8c58423
kghafari <kghafari@users.noreply.github.com>
2020-02-20 16:43:27 -0500

    first

---
+++ MVC_Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculator(int num1, int num2, string operation)
+        {
+            int result = 0;
+            if (operation == "addition")
+            {
+                result = num1 + num2;
+            }
+            else if (operation == "subtraction")
+            {
+                result = num1 - num2;
+            }
+            else if (operation == "multiplication")
+            {
+                result = num1 * num2;
+            }
+            else
+            {
+                result = num1 / num2;
+            }
+            return View("DisplayResult", result);
+        }
+
+        public IActionResult DisplayResult()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_Calculator/Models/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Calculator.Models
+{
+    public class Calculator
+    {
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+
+
+    }
+}
